356e2dbbe5699b4ea1441056fd3bdacb12ed7156
Seamile <lanhuermao@gmail.com>
2020-01-03 10:04:30 +0800

    添加了 User 模型

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    '''user 表的数据模型'''
+    phonenum = models.CharField(max_length=16, verbose_name='手机号')
+    nickname = models.CharField(max_length=32, verbose_name='昵称')
+    gender = models.CharField(max_length=10, verbose_name='性别')
+    birthday = models.DateField(default='1990-01-01', verbose_name='生日')
+    avatar = models.CharField(max_length=256, verbose_name='个人形象')
+    location = models.CharField(max_length=20, verbose_name='常居地')
