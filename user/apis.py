b9c59a9a79c5e54dfd597df9f683befc316544a6
Seamile <lanhuermao@gmail.com>
2020-01-03 14:57:21 +0800

    完成了登陆、注册接口

--- user/apis.py
+++ user/apis.py
@@ -1,21 +1,42 @@
 from django.http import JsonResponse
+from django.core.cache import cache
 
+from user.models import User
 from user import logics
 from common import stat
 
 
 def get_vcode(request):
     '''用户获取验证码'''
-    phonenum = request.GET.get('phonenum')
-    success = logics.send_sms(phonenum)
-    if success:
-        return JsonResponse({'code': stat.OK, 'data': None})
-    else:
-        return JsonResponse({'code': stat.SMS_ERR, 'data': None})
+    try:
+        phonenum = request.GET.get('phonenum')
+        success = logics.send_sms(phonenum)
+        if success:
+            return JsonResponse({'code': stat.OK, 'data': None})
+        else:
+            return JsonResponse({'code': stat.SMS_ERR, 'data': None})
+    except:
+        pass
 
 
 def submit_vcode(request):
-    return JsonResponse({})
+    '''检查短信验证码，同时进行登陆或者注册'''
+    phonenum = request.POST.get('phonenum')
+    vcode = request.POST.get('vcode')
+
+    key = 'Vcode-%s' % phonenum
+    cached_vcode = cache.get(key)
+    if vcode and vcode == cached_vcode:
+        try:
+            user = User.objects.get(phonenum=phonenum)  # 获取用户
+        except User.DoesNotExist:
+            user = User.objects.create(phonenum=phonenum, nickname=phonenum)  # 创建新用户
+
+        # 记录用户登陆状态
+        request.session['uid'] = user.id
+        return JsonResponse({'code': stat.OK, 'data': user.to_dict()})
+    else:
+        return JsonResponse({'code': stat.VCODE_ERR, 'data': None})
 
 
 def get_profile(request):
--- user/models.py
+++ user/models.py
@@ -3,9 +3,33 @@ from django.db import models
 
 class User(models.Model):
     '''user 表的数据模型'''
-    phonenum = models.CharField(max_length=16, verbose_name='手机号')
+    GENDERS = (
+        ('male', '男性'),
+        ('female', '女性'),
+    )
+    LOCATION = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('深圳', '深圳'),
+        ('成都', '成都'),
+        ('西安', '西安'),
+        ('武汉', '武汉'),
+        ('沈阳', '沈阳'),
+    )
+    phonenum = models.CharField(max_length=16, unique=True, verbose_name='手机号')
     nickname = models.CharField(max_length=32, verbose_name='昵称')
-    gender = models.CharField(max_length=10, verbose_name='性别')
+    gender = models.CharField(max_length=10, default='male', choices=GENDERS, verbose_name='性别')
     birthday = models.DateField(default='1990-01-01', verbose_name='生日')
     avatar = models.CharField(max_length=256, verbose_name='个人形象')
-    location = models.CharField(max_length=20, verbose_name='常居地')
+    location = models.CharField(max_length=20, default='北京', choices=LOCATION, verbose_name='常居地')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'phonenum': self.phonenum,
+            'nickname': self.nickname,
+            'gender': self.gender,
+            'birthday': str(self.birthday),
+            'avatar': self.avatar,
+            'location': self.location,
+        }
