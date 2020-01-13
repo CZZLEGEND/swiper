31401354c002024a87a4d931dda1a37421d9fa17
Seamile <lanhuermao@gmail.com>
2020-01-13 11:56:06 +0800

    添加了 Social 相关的模型

--- social/models.py
+++ social/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Swiped(models.Model):
+    '''滑动记录'''
+    STYPES = (
+        ('like', '右滑'),
+        ('superlike', '上滑'),
+        ('dislike', '左滑'),
+    )
+    uid = models.IntegerField(verbose_name='用户 ID')
+    sid = models.IntegerField(verbose_name='被滑动用户的 ID')
+    stype = models.CharField(max_length=10, choices=STYPES, verbose_name='滑动的类型')
+    stime = models.DateTimeField(auto_now_add=True, verbose_name='滑动的时间')
+
+    class Meta:
+        unique_together = ('uid', 'sid')  # uid 与 sid 联合唯一
+
+
+class Friend(models.Model):
+    '''好友表'''
+    uid1 = models.IntegerField(verbose_name='用户 ID')
+    uid2 = models.IntegerField(verbose_name='用户 ID')
+
+    class Meta:
+        unique_together = ('uid1', 'uid2')  # uid1 与 uid2 联合唯一
